3ca3a4ec092327680535e42ca5565ce7e6d060ac
JoaoVicPugliesi <mrlanguages62@gmail.com>
2025-03-26 07:41:10 -0300

    Add two variables inside .env PORT and HOST. Also, adding logger to fastify instance. Change server.ts providing terminal signals. Create first test route

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,27 @@
-import { app } from "./app";
+import { app } from './app';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const PORT: number = parseInt(process.env.PORT as string) || 5000;
+const HOST: string = process.env.HOST as string || '127.0.0.1';
+
 async function server(): Promise<void> {
     try {
-        const isListening: string = await app.listen({
-            port: 8000
+        await app.listen({
+            port: PORT,
+            host: HOST
         });
 
-        if (isListening) console.log(`🚀 server is running on http://localhost:8000`); 
-    } catch (error) {
-        console.log(error);
+        app.log.info(`🚀 server is running on http://${HOST}:${PORT}`);
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
     }
 }
 
 server();
 
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Hello World' });
+});
